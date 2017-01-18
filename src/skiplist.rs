17393a9caf970c4479853782017cdfe8812f093a
Thomas Krause <thomaskrause@posteo.de>
2017-01-18 13:28:37 +0100

    Begin to implement the initialization of a new SkipList

--- src/skiplist.rs
+++ src/skiplist.rs
@@ -14,19 +14,46 @@
 
 use std::collections::LinkedList;
 
+struct ProxyNode {
+    keys : Vec<u32>
+}
+
 struct SkipList {
-    max_level: u8,
-    skip: u8,
+    max_level: usize,
+    skip: usize,
+    proxy_lane: Vec<ProxyNode>,
+    fast_lanes: Vec<u32>,
     nodes: LinkedList<u32>,
+    items_per_level: Vec<usize>,
+    start_of_fast_lanes : Vec<usize>
 }
 
 impl SkipList {
 
-    fn new(max_level: u8, skip: u8) -> SkipList {
+    pub fn new(max_level: usize, skip: usize) -> SkipList {
+
+        // build the fast lanes
+        let mut items_per_level = vec![0; max_level];
+        let mut start_of_fast_lanes = vec![0; max_level];
+        let mut flane_size = 16 as usize;
+
+        items_per_level[max_level-1] = flane_size;
+        start_of_fast_lanes[max_level-1] = 0;
+
+        for level in (max_level -2)..0 {
+            items_per_level[level] = items_per_level[level+1] * skip;
+            start_of_fast_lanes[level] = start_of_fast_lanes[level+1] + items_per_level[level+1];
+            flane_size += items_per_level[level];
+        }
+
         SkipList {
             max_level: max_level,
             skip: if skip > 1 {skip} else {2},
-            nodes : LinkedList::new()
+            proxy_lane: Vec::with_capacity(flane_size),
+            fast_lanes: vec![u32::max_value(); flane_size],
+            nodes : LinkedList::new(),
+            items_per_level: items_per_level,
+            start_of_fast_lanes: start_of_fast_lanes,
         }
     }
 
@@ -41,7 +68,7 @@ mod tests {
 
     #[test]
     fn insert() {
-//        let slist = cssl::SkipList::new();
+        let slist = SkipList::new();
         
     }
 }
